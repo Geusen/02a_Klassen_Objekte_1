e38e8cbddb6ef99d57522d78470e4b9163895bf6
David <paulussen86@gmail.com>
2024-01-24 11:12:00 +0100

    update

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,19 @@ package com.cc.java;
 
 public class Cat {
 
+    String name;
+    String furColor;
+    int age;
+
+    public void tellYourName(){
+
+            System.out.println(this.name);
+
+
+    }
+
+
+
 
 
 
