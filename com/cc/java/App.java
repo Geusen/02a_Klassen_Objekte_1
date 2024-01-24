e6d33ed2574181abf6dbcc90fc40bd3bd2f34a04
David <paulussen86@gmail.com>
2024-01-24 11:50:48 +0100

    Klassen und wie ich mehrere Objekte aus Klassen bilde und ausgebe.

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,18 +4,18 @@ public class App {
     
     public static void main(String[] args) {
 
-        // Objekt erzeugen
+        // Objekt 1 erzeugen
         Cat cat = new Cat();    // Instanziierung
         System.out.println(cat);
 
-        // Wertzuweisung
+        // Wertzuweisung Objekt 1
         cat.name = "Alonzo";
         cat.furColor = "grey";
         cat.age = 35;
 
     
 
-       // Ausgabe 
+       // Ausgabe Objekt 1
         System.out.println(cat.name);
         System.out.println(cat.furColor);
         System.out.println(cat.age);
@@ -23,18 +23,19 @@ public class App {
 
         System.out.println("-------");
 
+        // Objekt 2 erzeugen
         Cat cat2 = new Cat();
         System.out.println(cat2);
-        
 
-         // Wertzuweisung
+
+         /// Wertzuweisung Objekt 2
          cat2.name = "Grizabella";
          cat2.furColor = "white";
          cat2.age = 29;
  
      
  
-        // Ausgabe 
+        // Ausgabe Objekt 2
          System.out.println(cat2.name);
          System.out.println(cat2.furColor);
          System.out.println(cat2.age);
